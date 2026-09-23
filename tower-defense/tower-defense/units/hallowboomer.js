75054c541bc87c0c180844f706655309a918ccbf
VuGiaLam2012 <vugialam2012@gmail.com>
2026-09-23 21:20:14 +0700

    Add files via upload

---
+++ tower-defense/tower-defense/units/hallowboomer.js
@@ -0,0 +1,12 @@
+import { createTdsTower } from './tds-tower.js';
+
+export const HallowboomerTower = createTdsTower({
+  name: 'Hallowboomer',
+  range: 2.7,
+  fireRate: 1,
+  dmg: 44,
+  cost: 450,
+  color: '#fbbf24',
+  weapon: 'grenade',
+  bodyScale: 1
+});
